f21e02ac27076611e7d407bd4d4a7ca2d3277a8f
roarinsightcorp <roarinsightcorp@gmail.com>
2026-09-24 14:37:58 +0100

    Configure Django WSGI for production

--- backend/config/wsgi.py
+++ backend/config/wsgi.py
@@ -13,7 +13,7 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault(
     "DJANGO_SETTINGS_MODULE",
-    "config.settings.development",
+    "config.settings.production",
 )
 
 application = get_wsgi_application()
